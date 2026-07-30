8e82c9bcae9e05f9f50d41488d62526bf1f67729
lakshmi2006511 <dhanalakshmikoppisetti11@gmail.com>
2026-07-30 14:38:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
-
+import os
 from sqlalchemy import create_engine
 # connection for database
  
 from sqlalchemy.orm import sessionmaker, declarative_base
  
  
-DATABASE_URL = "mysql+pymysql://root:30890349@localhost:3306/ecommerce_db"
-# http://localhost:8000
+# Reads DATABASE_URL from Render's Environment Variables.
+# Falls back to the Aiven URL below only for local testing.
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "mysql+pymysql://avnadmin:AVNS_yOUyno51nShOuRZ-1Q8@mysql-3be8f1b9-dhanalakshmi33.e.aivencloud.com:18494/defaultdb?ssl_mode=REQUIRED"
+)
 engine = create_engine(DATABASE_URL)
  
 SessionLocal = sessionmaker(
