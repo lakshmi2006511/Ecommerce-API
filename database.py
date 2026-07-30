cc603c424607ea291aba06fc9473170e69cffc25
lakshmi2006511 <dhanalakshmikoppisetti11@gmail.com>
2026-07-30 14:46:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Falls back to the Aiven URL below only for local testing.
 DATABASE_URL = os.environ.get(
     "DATABASE_URL",
-    "mysql+pymysql://avnadmin:AVNS_yOUyno51nShOuRZ-1Q8@mysql-3be8f1b9-dhanalakshmi33.e.aivencloud.com:18494/defaultdb?ssl_mode=REQUIRED"
+    "mysql+pymysql://avnadmin:AVNS_yOUyno51nShOuRZ-1Q8@mysql-3be8f1b9-dhanalakshmi33.e.aivencloud.com:18494/defaultdb"
 )
 engine = create_engine(DATABASE_URL)
  
