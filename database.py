2aa0b8584ef1fa5e45c5d8263920750f1cda6d40
lakshmi2006511 <dhanalakshmikoppisetti11@gmail.com>
2026-08-03 18:24:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Render dashboard లో Environment Variable గా DATABASE_URL పెట్టు
-DATABASE_URL = os.environ["mysql://avnadmin:AVNS_yOUyno51nShOuRZ-1Q8@mysql-3be8f1b9-dhanalakshmi33.e.aivencloud.com:18494/defaultdb"]
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
