f0744d61823b5932a6aa766c45d27aa2fba2c6c6
lakshmi2006511 <dhanalakshmikoppisetti11@gmail.com>
2026-08-03 16:23:57 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,11 @@
 import os
 from sqlalchemy import create_engine
-# connection for database
- 
 from sqlalchemy.orm import sessionmaker, declarative_base
- 
- 
-# Reads DATABASE_URL from Render's Environment Variables.
-# Falls back to the Aiven URL below only for local testing.
-DATABASE_URL = os.environ.get(
-    "DATABASE_URL",
-    "mysql+pymysql://avnadmin:AVNS_yOUyno51nShOuRZ-1Q8@mysql-3be8f1b9-dhanalakshmi33.e.aivencloud.com:18494/defaultdb"
-)
-engine = create_engine(DATABASE_URL)
- 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
- 
+
+# Render dashboard లో Environment Variable గా DATABASE_URL పెట్టు
+DATABASE_URL = os.environ["mysql://avnadmin:AVNS_yOUyno51nShOuRZ-1Q8@mysql-3be8f1b9-dhanalakshmi33.e.aivencloud.com:18494/defaultdb"]
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-# provides a base class for models of sql tables
