902d863b65e5202216a068591ae98e8218a3c6b0
lakshmi2006511 <dhanalakshmikoppisetti11@gmail.com>
2026-08-03 12:20:05 +0530

    https://github.com/lakshmi2006511/Ecommerce-API.git

--- database.py
+++ database.py
@@ -1,19 +1,24 @@
-
+import os
 from sqlalchemy import create_engine
-# connection for database
- 
 from sqlalchemy.orm import sessionmaker, declarative_base
- 
- 
-DATABASE_URL = "mysql+pymysql://root:30890349@localhost:3306/ecommerce_db"
-# http://localhost:8000
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set.")
+
+DATABASE_URL ="mysql+pymysql://avnadmin:AVNS_yOUyno51nShOuRZ-1Q8@mysql-3be8f1b9-dhanalakshmi33.e.aivencloud.com:18494/defaultdb".strip().strip('"').strip("'")
+
 engine = create_engine(DATABASE_URL)
- 
+
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
- 
+
 Base = declarative_base()
 # provides a base class for models of sql tables
